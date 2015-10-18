e4b6b7cc3b3c4562a9fad2e5c891c6453e0f9092
Alexey Chernov <4ernov@gmail.com>
2015-10-18 18:54:48 +0300

    Remove unnecessary zeroing out of output buffer

--- example/main.cpp
+++ example/main.cpp
@@ -10,9 +10,6 @@ int main(int, char**)
 	double pi = 0.1;
 	char buffer[128];
 
-	for (int i = 0; i < 128; ++i)
-		buffer[i] = 0;
-
 	dtoa(pi, buffer);
 	std::cout << "pi: " << pi << ", buffer: " << buffer << std::endl;
 
