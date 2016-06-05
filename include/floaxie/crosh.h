a9c1a0f4246a41ace6b9566cdf2c2db9d6b59d0f
Alexey Chernov <4ernov@gmail.com>
2016-06-05 13:50:49 +0300

    Use compile-time generated pow10 result array
    Generate results of power-of-10 up to the necessary maximum power
    in compile-time, with template magic, and just get it with access
    by index in runtime, rather than calculating the actual power.

--- include/floaxie/crosh.h
+++ include/floaxie/crosh.h
@@ -22,11 +22,11 @@
 #include <cassert>
 
 #include <floaxie/diy_fp.h>
-#include <floaxie/dword_diy_fp.h>
 #include <floaxie/static_pow.h>
 #include <floaxie/k_comp.h>
 #include <floaxie/cached_power.h>
 #include <floaxie/bit_ops.h>
+#include <floaxie/pow10.h>
 
 #include <iostream>
 #include <bitset>
@@ -160,15 +160,6 @@ namespace floaxie
 		return frac;
 	}
 
-	inline diy_fp::mantissa_storage_type pow10(std::size_t pow)
-	{
-		diy_fp::mantissa_storage_type ret(1);
-		for (std::size_t i = 0; i < pow; ++i)
-			ret *= 10;
-
-		return ret;
-	}
-
 	inline std::size_t digit_decomp(diy_fp& w, const char* buffer, std::size_t len)
 	{
 		diy_fp::mantissa_storage_type f(0);
@@ -177,20 +168,11 @@ namespace floaxie
 		// read up to kappa decimal digits to mantissa
 		const std::size_t kappa(std::min(decimal_q, len));
 
-		std::cout << "kappa: " << kappa << std::endl;
-		std::cout << "1000 == " << pow10(3) << std::endl;
-
 		for (std::size_t curr_char = 0; curr_char < kappa; ++curr_char)
 		{
 			const char digit = buffer[curr_char] - '0';
-			f += digit * pow10(kappa - curr_char - 1);
-// 			std::cout << "digit: " << char(digit + '0') << ", curr_char: " << curr_char << ", pow: " << kappa - curr_char - 1 << std::endl;
-// 			std::cout << "mantissa binary: " << std::bitset<64>(f) << std::endl;
-// 			std::cout << "f: " << f << std::endl;
+			f += digit * pow10<diy_fp::mantissa_storage_type, decimal_q>(kappa - curr_char - 1);
 		}
-
-		std::cout << "before normalization, mantissa binary: " << std::bitset<64>(f) << std::endl;
-		std::cout << "before normalization, f: " << f <<", e: "<< e << std::endl;
 
 		// normalize interim value
 		while (!highest_bit(f))
@@ -198,9 +180,6 @@ namespace floaxie
 			f <<= 1;
 			--e;
 		}
-
-		std::cout << "after normalization, mantissa binary: " << std::bitset<64>(f) << std::endl;
-		std::cout << "after normalization, f: " << f <<", e: "<< e << std::endl;
 
 		if (kappa < len)
 		{
@@ -210,10 +189,7 @@ namespace floaxie
 			// save some more bits restoring prefix of fraction
 			const unsigned char frac(extract_simple_fraction_4(buffer + kappa, len - kappa));
 
-			std::cout << "acc: " << std::bitset<8>(frac >> lsb_pow).to_string().substr(8 + e) << std::endl;
 			f |= frac >> lsb_pow;
-			std::cout << "after restore, mantissa binary: " << std::bitset<64>(f) << std::endl;
-			std::cout << "after restore, f: " << f <<", e: "<< e << std::endl;
 
 			// round correctly avoiding integer overflow, undefined behaviour, pain and suffering
 			bool accurate;
@@ -231,8 +207,6 @@ namespace floaxie
 					++e;
 				}
 			}
-			std::cout << "after roundup, mantissa binary: " << std::bitset<64>(f) << std::endl;
-			std::cout << "after roundup, f: " << f <<", e: "<< e << std::endl;
 		}
 
 		w = diy_fp(f, e);
@@ -240,92 +214,17 @@ namespace floaxie
 		return kappa;
 	}
 
-	inline dword_diy_fp precise_multiply4(const diy_fp& lhs, const diy_fp& rhs) noexcept
-	{
-// 		std::cout << "precise_multiply4" << std::endl;
-// 		std::cout << "lhs:      " << lhs << std::endl;
-// 		std::cout << "rhs:      " << rhs << std::endl;
-		constexpr auto mask_32 = 0xffffffff;
-
-		const diy_fp::mantissa_storage_type a = lhs.mantissa() >> 32;
-		const diy_fp::mantissa_storage_type b = lhs.mantissa() & mask_32;
-		const diy_fp::mantissa_storage_type c = rhs.mantissa() >> 32;
-		const diy_fp::mantissa_storage_type d = rhs.mantissa() & mask_32;
-
-		const diy_fp::mantissa_storage_type ac = a * c;
-		const diy_fp::mantissa_storage_type bc = b * c;
-		const diy_fp::mantissa_storage_type ad = a * d;
-		const diy_fp::mantissa_storage_type bd = b * d;
-
-		const diy_fp::mantissa_storage_type rz = bd;
-		const diy_fp::mantissa_storage_type ry = (rz >> 32) + (ad & mask_32) + (bc & mask_32);
-		const diy_fp::mantissa_storage_type rx = (ry >> 32) + (ad >> 32) + (bc >> 32) + (ac & mask_32);
-		const diy_fp::mantissa_storage_type rw = (rx >> 32) + (ac >> 32);
-
-		dword_diy_fp::mantissa_storage_type f
-		{
-			(((rw & mask_32) << 32) | (rx & mask_32)),
-			((ry & mask_32) << 32) | (rz & mask_32)
-		};
-
-// 		std::cout << "result h: " << std::bitset<64>(f.higher()) << std::endl;
-// 		std::cout << "result l: " << std::bitset<64>(f.lower()) << std::endl;
-
-		return dword_diy_fp(f, lhs.exponent() + rhs.exponent());
-	}
-
-	inline diy_fp narrow_down(const dword_diy_fp& v) noexcept
-	{
-		constexpr std::size_t word_width(bit_size<dword_diy_fp::mantissa_storage_type::word_type>());
-
-		return diy_fp(v.mantissa().higher() + highest_bit(v.mantissa().lower()), v.exponent() + word_width);
-	}
-
 	template<typename FloatType> FloatType crosh(const char* buffer, int len, int K, bool* accurate)
 	{
-		std::cout << "buffer: " << buffer << std::endl;
-		std::cout << "len: " << len << std::endl;
-		std::cout << "initial K: " << K << std::endl;
-		diy_fp D, w;
+		diy_fp D;
 		const int kappa(digit_decomp(D, buffer, len));
 		K += len - kappa;
-// 		K = -18;
-		std::cout << "D: " << D << std::endl;
-		std::cout << "K: " << K << std::endl;
 
+		diy_fp w = D;
 		if (K)
-		{
-			const diy_fp& c_mk(cached_power(K)), c_rmk(cached_power(-K));
-			std::cout << "c_mk: " << c_mk << ", f: " << c_mk.mantissa() << std::endl;
-// 			w *= c_mk;
-// 			D.precise_multiply2(c_mk);
-// 			diy_fp rh, rl;
+			w *= cached_power(K);
 
-			auto r = precise_multiply4(D, c_mk);
-			r.normalize();
-
-// 			std::cout << "r:  " << r << std::endl;
-// 			auto rc = D * c_mk;
-// 			rc.normalize();
-// 			std::cout << "rc: " << rc << std::endl;
-			w = narrow_down(r);
-// 			w = rc;
-
-// 			auto wc = rc;
-// 			std::cout << "c_mk * c_rmk: " << precise_multiply4(c_mk, c_rmk) << std::endl;
-// 			std::cout << "~w: " << r << std::endl;
-// 			w = precise_round(rh, rl, D, c_rmk);
-			std::cout << "w:  " << w << std::endl;
-// 			std::cout << "wc: " << wc << std::endl;
-// 			std::cout << "rc: " << rc << std::endl;
-// 			w.normalize();
-// 			w.normalize();
-		}
-		else
-		{
-			w = D;
-			w.normalize();
-		}
+		w.normalize();
 
 		return w.downsample<FloatType>(accurate);
 	}
