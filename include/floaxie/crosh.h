668a8318bb2627c974b692bc41289a03f3bce091
Alexey Chernov <4ernov@gmail.com>
2016-07-31 23:18:20 +0300

    Debugging and fine-tuning new duty cycle of crosh
    Make the necessary tweaks in new joint parse-calculate cycle to
    behave it the same as the previous version.
    
    Convert fraction binary numerator calculation to template-based
    code, which is partially calculated at compile-time, rather than
    hardcoded function.

--- include/floaxie/crosh.h
+++ include/floaxie/crosh.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Alexey Chernov <4ernov@gmail.com>
+ * Copyright 2015, 2016 Alexey Chernov <4ernov@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@
 #include <floaxie/cached_power.h>
 #include <floaxie/bit_ops.h>
 #include <floaxie/pow10.h>
+#include <floaxie/fraction.h>
 
 #include <iostream>
 #include <bitset>
@@ -37,192 +38,36 @@ namespace floaxie
 {
 	constexpr std::size_t decimal_q(bit_size<diy_fp::mantissa_storage_type>() * lg_2);
 
-// 	inline char calculate_fraction_4(int numerator)
-// 	{
-// 		if (numerator >= 5000)
-// 		{
-// 			if (numerator >= 7500)
-// 			{
-// 				if (numerator >= 8750)
-// 				{
-// 					if (numerator >= 9375)
-// 						return 0x0f;
-// 					else
-// 						return 0x0e;
-// 				}
-// 				else
-// 				{
-// 					if (numerator >= 8125)
-// 						return 0x0d;
-// 					else
-// 						return 0x0c;
-// 				}
-// 			}
-// 			else
-// 			{
-// 				if (numerator >= 6250)
-// 				{
-// 					if (numerator >= 6875)
-// 						return 0x0b;
-// 					else
-// 						return 0x0a;
-// 				}
-// 				else
-// 				{
-// 					if (numerator >= 5625)
-// 						return 0x09;
-// 					else
-// 						return 0x08;
-// 				}
-// 			}
-// 		}
-// 		else
-// 		{
-// 			if (numerator >= 2500)
-// 			{
-// 				if (numerator >= 3750)
-// 				{
-// 					if (numerator >= 4325)
-// 						return 0x07;
-// 					else
-// 						return 0x06;
-// 				}
-// 				else
-// 				{
-// 					if (numerator >= 3125)
-// 						return 0x05;
-// 					else
-// 						return 0x04;
-// 				}
-// 			}
-// 			else
-// 			{
-// 				if (numerator >= 1250)
-// 				{
-// 					if (numerator >= 1875)
-// 						return 0x03;
-// 					else
-// 						return 0x02;
-// 				}
-// 				else
-// 				{
-// 					if (numerator >= 625)
-// 						return 0x01;
-// 					else
-// 						return 0x00;
-// 				}
-// 			}
-// 		}
-// 	}
-//
-// 	inline bool detect_round_up_bit(int numerator)
-// 	{
-// 		switch (numerator)
-// 		{
-// 			case 9375:
-// 			case 8750:
-// 			case 8125:
-// 			case 7500:
-// 			case 6875:
-// 			case 6250:
-// 			case 5625:
-// 			case 5000:
-// 			case 4325:
-// 			case 3750:
-// 			case 3125:
-// 			case 2500:
-// 			case 1875:
-// 			case 1250:
-// 			case 625:
-// 			case 0:
-// 				return false;
-// 			default:
-// 				return true;
-// 		}
-// 	}
-//
-// 	inline unsigned char extract_simple_fraction_4(const char* buffer, int len)
-// 	{
-// 		const unsigned int num = (len > 0 ? buffer[0] - '0' : 0) * static_pow<10, 3>() +
-// 				(len > 1 ? buffer[1] - '0' : 0) * static_pow<10, 2>() +
-// 				(len > 2 ? buffer[2] - '0' : 0) * static_pow<10, 1>() +
-// 				(len > 3 ? buffer[3] - '0' : 0);
-//
-// 		std::cout << "num: " << num << std::endl;
-//
-// 		unsigned char frac = calculate_fraction_4(num);
-// // 		const bool round_up = (frac & 0x1ul) && (len > 4 || detect_round_up_bit(num));
-// 		const bool round_up = (len > 4 || detect_round_up_bit(num));
-// 		std::cout << "round_up: " << round_up << std::endl;
-// 		frac <<= 1;
-// 		frac |= round_up;
-// 		auto frac_str(std::bitset<8>(frac).to_string().substr(3));
-// 		std::cout << "frac binary: " << frac_str << std::endl;
-//
-// 		return frac;
-// 	}
-//
-// 	inline std::size_t digit_decomp(diy_fp& w, const char* buffer, std::size_t len)
-// 	{
-// 		diy_fp::mantissa_storage_type f(0);
-// 		diy_fp::exponent_storage_type e(0);
-//
-// 		// read up to kappa decimal digits to mantissa
-// 		const std::size_t kappa(std::min(decimal_q, len));
-//
-// 		for (std::size_t curr_char = 0; curr_char < kappa; ++curr_char)
-// 		{
-// 			const char digit = buffer[curr_char] - '0';
-// 			f += digit * pow10<diy_fp::mantissa_storage_type, decimal_q>(kappa - curr_char - 1);
-// 		}
-//
-// 		// normalize interim value
-// 		while (!highest_bit(f))
-// 		{
-// 			f <<= 1;
-// 			--e;
-// 		}
-//
-// 		if (kappa < len)
-// 		{
-// 			assert(e >= -4);
-// 			const std::size_t lsb_pow(5 + e);
-//
-// 			// save some more bits restoring prefix of fraction
-// 			const unsigned char frac(extract_simple_fraction_4(buffer + kappa, len - kappa));
-//
-// 			f |= frac >> lsb_pow;
-//
-// 			// round correctly avoiding integer overflow, undefined behaviour, pain and suffering
-// 			bool accurate;
-// 			const bool should_round_up(round_up(frac, lsb_pow, &accurate));
-// 			if (should_round_up)
-// 			{
-// 				if (f < std::numeric_limits<diy_fp::mantissa_storage_type>::max())
-// 				{
-// 					++f;
-// 				}
-// 				else
-// 				{
-// 					f >>= 1;
-// 					++f;
-// 					++e;
-// 				}
-// 			}
-// 		}
-//
-// 		w = diy_fp(f, e);
-//
-// 		return kappa;
-// 	}
+	constexpr std::size_t fraction_binary_digits(4);
+	constexpr std::size_t fraction_decimal_digits(4);
 
-// 	constexpr std::size_t max_buffer_length(32);
 	constexpr std::size_t offset = 2048;
 
-	// read up to kappa decimal digits (no more digits needed)
-// 	constexpr std::size_t kappa(decimal_q);
-	constexpr std::size_t frac_width(5);
+	constexpr std::size_t frac_width(4);
 	constexpr std::size_t exp_width(3);
+
+	template<std::size_t kappa>
+	inline diy_fp::mantissa_storage_type extract_fraction_digits(const char* str)
+	{
+		std::array<unsigned char, kappa> parsed_digits;
+		parsed_digits.fill(0);
+
+		for (std::size_t pos = 0; pos < kappa; ++pos)
+		{
+			const char c = str[pos] - '0';
+			if (c >= 0 && c <= 9)
+				parsed_digits[pos] = c;
+			else
+				break;
+		}
+
+		diy_fp::mantissa_storage_type result(0);
+		std::size_t pow(0);
+		for (auto rit = parsed_digits.rbegin(); rit != parsed_digits.rend(); ++rit)
+			result += (*rit) * pow10<diy_fp::mantissa_storage_type, frac_width>(pow++);
+
+		return result;
+	}
 
 	template<std::size_t kappa, bool no_dot, bool calc_frac>
 	inline diy_fp::mantissa_storage_type parse_digits(const char* str, const char** str_end, bool* sign, int* K, unsigned char* frac = nullptr)
@@ -237,6 +82,7 @@ namespace floaxie
 			*frac = 0;
 
 		bool dot_set(false);
+		bool frac_calculated(!calc_frac);
 		std::size_t pow_gain(0);
 		std::size_t zero_substring_length(0), fraction_digits_count(0);
 
@@ -249,8 +95,11 @@ namespace floaxie
 			switch (c)
 			{
 			case '0':
-				++zero_substring_length;
-				pow_gain += !dot_set;
+				if (!parsed_digits.empty() || dot_set)
+				{
+					++zero_substring_length;
+					pow_gain += !dot_set;
+				}
 				break;
 
 			case '1':
@@ -262,7 +111,7 @@ namespace floaxie
 			case '7':
 			case '8':
 			case '9':
-				if (parsed_digits.size() + zero_substring_length <= kappa)
+				if (parsed_digits.size() + zero_substring_length < kappa)
 				{
 					if (zero_substring_length)
 					{
@@ -277,12 +126,13 @@ namespace floaxie
 				}
 				else
 				{
-					if (calc_frac)
+					if (!frac_calculated)
 					{
-						const char* frac_str_end;
-						bool frac_sign;
-						int frac_K;
-						*frac = parse_digits<frac_width, true, false>(str + pos, &frac_str_end, &frac_sign, &frac_K);
+						auto tail = extract_fraction_digits<frac_width>(str + pos - zero_substring_length);
+						*frac = convert_numerator<fraction_decimal_digits, fraction_binary_digits>(tail);
+
+						frac_calculated = true;
+
 					}
 					pow_gain += !dot_set;
 				}
@@ -329,14 +179,11 @@ namespace floaxie
 		diy_fp w(parse_digits<decimal_q, false, true>(str, str_end, sign, K, &frac), 0);
 		w.normalize();
 
-		std::cout << "frac: " << int(frac) << std::endl;
-
 		// extract additional binary digits and round up gently
 		if (frac)
 		{
-			std::cout << "exp: " << w.exponent() << std::endl;
 			assert(w.exponent() >= -4);
-			const std::size_t lsb_pow(5 + w.exponent());
+			const std::size_t lsb_pow(4 + w.exponent());
 
 			diy_fp::mantissa_storage_type f(w.mantissa());
 			f |= frac >> lsb_pow;
@@ -346,7 +193,6 @@ namespace floaxie
 			// round correctly avoiding integer overflow, undefined behaviour, pain and suffering
 			bool accurate;
 			const bool should_round_up(round_up(frac, lsb_pow, &accurate));
-			std::cout << "should_round_up: " << should_round_up << std::endl;
 			if (should_round_up)
 			{
 				++w;
@@ -369,6 +215,7 @@ namespace floaxie
 		bool sign;
 		int K;
 		int value(parse_digits<exp_width, false, false>(str, str_end, &sign, &K));
+		value *= pow10<int, exp_width>(K);
 		return sign ? value : -value;
 	}
 
@@ -378,7 +225,7 @@ namespace floaxie
 		int K(0);
 		diy_fp w(parse_mantissa(str, str_end, sign, &K));
 
-		K += parse_exponent(*str_end, str_end);
+		K += parse_exponent(*str_end, str_end);;
 
 		if (K)
 			w *= cached_power(K);
---
+++ include/floaxie/fraction.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2015, 2016 Alexey Chernov <4ernov@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FLOAXIE_FRACTION_H
+#define FLOAXIE_FRACTION_H
+
+#include <cstddef>
+
+#include <floaxie/static_pow.h>
+
+namespace floaxie
+{
+	template<
+	typename T,
+	std::size_t decimal_digits,
+	std::size_t binary_digits,
+	std::size_t current_binary_digit,
+	bool terminal = (binary_digits == current_binary_digit)>
+	struct fraction_converter;
+
+	template<typename T,
+	std::size_t decimal_digits,
+	std::size_t binary_digits,
+	std::size_t current_binary_digit>
+	struct fraction_converter<T, decimal_digits, binary_digits, current_binary_digit, false>
+	{
+		static T convert(T decimal_numerator)
+		{
+			constexpr T numerator(static_pow<10, decimal_digits>());
+			constexpr T denominator(static_pow<2, current_binary_digit>());
+			constexpr T decimal_fraction(numerator / denominator);
+
+			constexpr std::size_t shift_amount(binary_digits - current_binary_digit);
+
+			const T decision(decimal_numerator >= decimal_fraction);
+			const T residue(decimal_numerator - decision * decimal_fraction);
+
+			return (decision << shift_amount) |
+				fraction_converter<T, decimal_digits, binary_digits, current_binary_digit + 1>::convert(residue);
+		}
+	};
+
+	template<typename T,
+	std::size_t decimal_digits,
+	std::size_t binary_digits,
+	std::size_t current_binary_digit>
+	struct fraction_converter<T, decimal_digits, binary_digits, current_binary_digit, true>
+	{
+		static T convert(T decimal_numerator)
+		{
+			constexpr T numerator(static_pow<10, decimal_digits>());
+			constexpr T denominator(static_pow<2, current_binary_digit>());
+			constexpr T decimal_fraction(numerator / denominator);
+
+			return decimal_numerator >= decimal_fraction;
+		}
+	};
+
+	template<std::size_t decimal_digits, std::size_t binary_digits, typename T> inline T convert_numerator(T decimal_numerator)
+	{
+		return fraction_converter<T, decimal_digits, binary_digits, 1>::convert(decimal_numerator);
+	}
+}
+
+#endif // FLOAXIE_FRACTION_H
