2facc7242ddf38c194680cf9a5ef011236786f39
Alexey Chernov <4ernov@gmail.com>
2016-08-01 01:10:49 +0300

    Fresh notation on multiline template parameter lists

--- include/floaxie/fraction.h
+++ include/floaxie/fraction.h
@@ -23,19 +23,24 @@
 
 namespace floaxie
 {
-	template<
-	typename T,
-	std::size_t decimal_digits,
-	std::size_t binary_digits,
-	std::size_t current_binary_digit,
-	bool terminal = (binary_digits == current_binary_digit)>
+	template
+	<
+		typename T,
+		std::size_t decimal_digits,
+		std::size_t binary_digits,
+		std::size_t current_binary_digit,
+		bool terminal = (binary_digits == current_binary_digit)
+	>
 	struct fraction_converter;
 
-	template<typename T,
-	std::size_t decimal_digits,
-	std::size_t binary_digits,
-	std::size_t current_binary_digit>
-	struct fraction_converter<T, decimal_digits, binary_digits, current_binary_digit, false>
+	template
+	<
+		typename T,
+		std::size_t decimal_digits,
+		std::size_t binary_digits,
+		std::size_t current_binary_digit>
+		struct fraction_converter<T, decimal_digits, binary_digits, current_binary_digit, false
+	>
 	{
 		static T convert(T decimal_numerator)
 		{
@@ -53,11 +58,14 @@ namespace floaxie
 		}
 	};
 
-	template<typename T,
-	std::size_t decimal_digits,
-	std::size_t binary_digits,
-	std::size_t current_binary_digit>
-	struct fraction_converter<T, decimal_digits, binary_digits, current_binary_digit, true>
+	template
+	<
+		typename T,
+		std::size_t decimal_digits,
+		std::size_t binary_digits,
+		std::size_t current_binary_digit>
+		struct fraction_converter<T, decimal_digits, binary_digits, current_binary_digit, true
+	>
 	{
 		static T convert(T decimal_numerator)
 		{
