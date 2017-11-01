2df5f1dd148a99aacd1e7768f998f949f4a37b85
Alexey Chernov <4ernov@gmail.com>
2017-11-02 02:16:17 +0300

    Avoid variable name collision to avoid C4459 in Visual Studio

--- test/string_wrappers.cpp
+++ test/string_wrappers.cpp
@@ -8,20 +8,20 @@ using namespace floaxie;
 
 namespace
 {
-	const wstring str(L"0.25");
-	const float v = 0.5;
+	const wstring test_str(L"0.25");
+	const float test_val = 0.5;
 }
 
 int main(int, char**)
 {
-	const auto from_string_result = from_string<double>(str);
+	const auto from_string_result = from_string<double>(test_str);
 
 	if (from_string_result != 0.25)
 	{
 		return -1;
 	}
 
-	const auto to_string_result = floaxie::to_string(v);
+	const auto to_string_result = floaxie::to_string(test_val);
 
 	cout << "result: \"" << to_string_result << "\"" << endl;
 
