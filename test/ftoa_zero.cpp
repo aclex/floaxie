aclex/floaxie

commit 3742c9b5ae9b1f50a031348c553c0c32ca2f68c6

Change printing zero values
It's just "0" now, not "0.0".

include/floaxie/ftoa.h
@@ -121,11 +121,9 @@ namespace floaxie
 		else if (v == 0)
 		{
 			buffer[0] = '0';
-			buffer[1] = '.';
-			buffer[2] = '0';
 			buffer[3] = '\0';
 
-			return 3;
+			return 1;
 		}
 		else
 		{

test/ftoa_zero.cpp
@@ -15,7 +15,7 @@ int main(int, char**)
 {
 	char buf[max_buffer_size<decltype(test_val)>()];
 	ftoa(test_val, buf);
-	if (std::strcmp(buf, "0.0"))
+	if (std::strcmp(buf, "0"))
 	{
 		return -1;
 	}

commit 907128a62a384034c4af8ed0af9882f3b4846548

Fix printing of zero
3742c9b introduced a bug of putting the zero terminal at wrong place
when printing zero value. Fixing it now, along with the corresponding
test case.

Many thanks to @syoyo for the report and proposed patch!

Closes #3.

include/floaxie/ftoa.h
@@ -121,7 +121,7 @@ namespace floaxie
 		else if (v == 0)
 		{
 			buffer[0] = '0';
-			buffer[3] = '\0';
+			buffer[1] = '\0';
 
 			return 1;
 		}

test/ftoa_zero.cpp
@@ -14,10 +14,15 @@ namespace
 int main(int, char**)
 {
 	char buf[max_buffer_size<decltype(test_val)>()];
-	ftoa(test_val, buf);
+	const auto sz = ftoa(test_val, buf);
 	if (std::strcmp(buf, "0"))
 	{
 		return -1;
+	}
+
+	if (std::strlen(buf) != sz)
+	{
+		return -2;
 	}
 
 	return 0;